cc2e290f6d74cd70fb938be4868bd7596bf9d435
orenom <orenom@tutanota.com>
2021-05-28 18:51:17 +0200

    remove port from the startup log

--- src/wikiless.js
+++ src/wikiless.js
@@ -103,6 +103,6 @@ redis.on('error', (error) => {
 })
 
 if(config.https_enabled) {
-  https.listen(config.ssl_port, '::', () => console.log(`Wikiless running on https://${config.domain}:${config.ssl_port}`))
+  https.listen(config.ssl_port, '::', () => console.log(`Wikiless running on https://${config.domain}`))
 }
-http.listen(config.nonssl_port, '::', () => console.log(`Wikiless running on http://${config.domain}:${config.nonssl_port}`))
+http.listen(config.nonssl_port, '::', () => console.log(`Wikiless running on http://${config.domain}`))
