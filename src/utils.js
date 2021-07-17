4172d2df6cb6c5bbf2edc7821e5234e8e0ee0073
orenom <orenom@tutanota.com>
2021-07-17 10:55:47 +0200

    fix the dir name for the comment

--- src/utils.js
+++ src/utils.js
@@ -143,7 +143,7 @@ module.exports = function(redis) {
         const upload_wikimedia_regx = /((https:|http:|)\/\/?upload.wikimedia.org)/gm
         data.html = data.html.replace(upload_wikimedia_regx, '/media')
         
-        // replace maps.wikimedia.org with /media
+        // replace maps.wikimedia.org with /media/maps_wikimedia_org
         const maps_wikimedia_regx = /((https:|http:|)\/\/?maps.wikimedia.org)/gm
         data.html = data.html.replace(maps_wikimedia_regx, '/media/maps_wikimedia_org')
         
